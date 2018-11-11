187a5c23374ee5dab44df5fd9a1443324660aab1
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-11-11 14:51:22 +0900

    edit-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :top, only: :index
+  resources :users, only: :show
+  resources :video, only: :show do
+    collection do
+      get 'genre_search'
+      get 'chef_search'
+      get 'keyword_search'
+    end
+  end
+  get 'video/chef_search_video/:chef_id' => "video#chef_search_video"
+  resources :article, only: [:index,:show] do
+    collection do
+      get 'genre_search'
+      get 'keyword_search'
+    end
+  end
+  root 'top#index'
 end
