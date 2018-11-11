c63a340fdc56dbe83b5470061b5a4b5324099ffc
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-11-11 14:47:03 +0900

    create-video-controller

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,2 +1,16 @@
 class VideoController < ApplicationController
+  def show
+  end
+
+  def genre_search
+  end
+
+  def keyword_search
+  end
+
+  def chef_search
+  end
+
+  def chef_search_video
+  end
 end
