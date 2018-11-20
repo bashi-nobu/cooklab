d49c07bd4c90c03dabcb9239e8526f814c565d42
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-11-20 21:20:26 +0900

    robocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  #has_one = 関連付け  inverse_of = 双方の関連付け
   has_one :userProfile, dependent: :destroy, inverse_of: :user
 
   accepts_nested_attributes_for :userProfile, update_only: true
