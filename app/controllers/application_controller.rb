c328d85a9bf58d7e40acda81cda921e9ad9a04a2
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-11-21 21:28:09 +0900

    edit strong-parameter
    deviseのストロングパラメーターを有効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     #devise_parameter_sanitizer = 許可するパラメータを追加（railsのバージョンによって書き方が異なるので注意）
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:userProfile_attributes => [:user_id, :name, :sex, :work_place, :job, :specialized_field, :location, :birthday]])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:userinfo_attributes => [:user_id, :name, :sex, :work_place, :job, :specialized_field, :location, :birthday]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:userProfile_attributes => [:user_id, :sex, :work_place, :job, :specialized_field, :location, :birthday]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:userinfo_attributes => [:user_id, :sex, :work_place, :job, :specialized_field, :location, :birthday]])
   end
 end
