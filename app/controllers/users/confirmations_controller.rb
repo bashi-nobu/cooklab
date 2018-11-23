e3fb8cc2b4c93d7f6ffc2703f65ff1e17350219a
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-11-23 20:03:13 +0900

    setting mail_auth
    メール認証機能&確認メールのテンプレート作成

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -12,9 +12,18 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      sign_in(resource) # <= THIS LINE ADDED
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+    end
+  end
 
   # protected
 
@@ -27,4 +36,10 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # def after_confirmation_path_for(resource_name, resource)
   #   super(resource_name, resource)
   # end
+
+  protected
+
+  def after_confirmation_path_for(resource_name, resource)
+    root_path
+  end
 end
