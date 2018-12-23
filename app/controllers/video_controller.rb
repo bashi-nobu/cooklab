78a6d619446fd373def5cd804b406458a51ead89
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-12-23 16:10:39 +0900

    setting view & controller file

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,5 +1,6 @@
 class VideoController < ApplicationController
   def show
+    @chefs = Chef.where(id: 1)
   end
 
   def genre_search
@@ -9,8 +10,23 @@ class VideoController < ApplicationController
   end
 
   def chef_search
+    @search_word = params_permit_chef_search[:search]
+    @chefs = Chef.chef_search(@search_word).page(params[:page]).per(5)
+    @recommend_chefs = Chef.select("name") #後ほど修正
+    @search_patarn = 'chef-search'
+    @search_path == '#'
   end
 
   def chef_search_video
+    @chef = Chef.find(params[:chef_id])
+    @recommend_chefs = Chef.select("name") #後ほど修正
+    @search_patarn = 'chef-video'
+    @search_path = '/video/chef_search'
+  end
+
+  private
+
+  def params_permit_chef_search
+    params.permit(:search)
   end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,7 @@
 class Chef < ApplicationRecord
   mount_uploader :chef_avatar, ChefAvatarUploader
+
+  def self.chef_search(chef_search_name)
+    Chef.where("name LIKE :search_name OR phonetic LIKE :search_name", search_name: "%#{chef_search_name}%")
+  end
 end
