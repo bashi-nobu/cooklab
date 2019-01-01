d3b8f6a61e364cca99c73551b5d78934ff93f8c4
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-01 15:54:14 +0900

    rename column charges tagble

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_061800) do
+ActiveRecord::Schema.define(version: 2019_01_01_064528) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_01_01_061800) do
     t.bigint "user_id"
     t.bigint "video_id"
     t.integer "price", null: false
-    t.string "product_id", null: false
+    t.string "payjp_charge_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_charges_on_user_id"
