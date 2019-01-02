b57c30edd2f30e271ee7b107ff474edbab692510
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-02 19:31:08 +0900

    setting article crud active admin

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
   mount_uploader :thumbnail, ArticleThumbnailUploader
+  with_options presence: true do
+    validates :title
+    validates :contents
+    validates :thumbnail
+  end
 end
