f9fc1d3437adc2ddc72e187f630918a5280e2c67
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-05 16:13:13 +0900

    reflect new&like order list data top_page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,18 @@
 class TopController < ApplicationController
   def index
-    @videos = Video.all.limit(10)
-    @articles = Article.all.limit(10)
+    @new_videos = Video.all.order("created_at desc").limit(5)
+    @like_videos = make_like_video_list
+    @articles = Article.all.order("created_at desc").limit(5)
+  end
+
+  private
+
+  def make_like_video_list
+    like_videos = VideoLike.group(:video_id).order('count(video_id) desc').pluck(:video_id)
+    like_video_list = []
+    like_videos.each do |lv|
+      like_video_list << Video.find(lv)
+    end
+    return like_video_list
   end
 end
