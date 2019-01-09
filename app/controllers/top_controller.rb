f4183001a4be16b872c6e7f1849d42a719a5cdc7
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-09 19:06:03 +0900

    correction

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
     @new_videos = Video.all.order("created_at desc").limit(5)
-    @like_videos =Video.all.order("like_count desc").limit(5)
+    @like_videos = Video.all.order("like_count desc").limit(5)
     @new_articles = Article.all.order("created_at desc").limit(5)
   end
 
