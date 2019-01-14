ce057eb08eec26962b23af38cd111bba6fbb1d98
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-14 22:51:35 +0900

    correction

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,9 +1,6 @@
 $(document).on('turbolinks:load', function(){
-  $('body').on("click","#navToggle",function(e){
-    $('.header').toggleClass('openNav');
-    $("#overlay").fadeToggle(100);/*ふわっと表示*/
-  });
-  $('body').on("click","#overlay",function(e){
+  $('body').on("click","#navToggle, #overlay",function(e){
+    alert('aaaaaa');
     $('.header').toggleClass('openNav');
     $("#overlay").fadeToggle(100);/*ふわっと表示*/
   });
