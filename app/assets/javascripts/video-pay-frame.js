ebc9ad7687d1e5edc44ff6ea3bcef97197414239
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-14 23:18:01 +0900

    correction

--- app/assets/javascripts/video-pay-frame.js
+++ app/assets/javascripts/video-pay-frame.js
@@ -1,14 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $(".video__view__vimeo").on("click", "#video-pay-confirmation-btn", function(e){
-    $("#video-overlay").stop().fadeToggle(500);
-    $(".video-pay-frame").stop().fadeToggle(500);
+    $("#video-overlay").stop().fadeToggle(300);
+    $(".video-pay-frame").stop().fadeToggle(300);
   });
-  $("body").on("click", "#video-overlay", function(e){
-    $("#video-overlay").stop().fadeToggle(500);
-    $(".video-pay-frame").stop().fadeToggle(500);
-  });
-  $('.video-pay-frame').on("click", "#video-pay-cancel", function(e){
-    $("#video-overlay").stop().hide();
-    $(".video-pay-frame").stop().hide();
+  $("body").on("click", "#video-overlay, #video-pay-cancel", function(e){
+    $("#video-overlay").stop().fadeToggle(300);
+    $(".video-pay-frame").stop().fadeToggle(300);
   });
 });
