ea2c09d91a2b7456a49ec0e1bd30dfd4e662c5d5
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-16 22:07:15 +0900

    setting customer contact system

--- app/assets/javascripts/video-pay-btn.js
+++ app/assets/javascripts/video-pay-btn.js
@@ -1,5 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(".video-pay-frame").on("click", "#video-pay-btn", function(e){
+    $(this).prop('disabled', true).css("opacity", 0.6);
     var video_id = $(this).attr('data-video-num');
     var price = $(this).attr('data-video-price');
     // 支払処理
@@ -15,7 +16,8 @@ $(document).on('turbolinks:load', function() {
     // ajax処理が成功した場合の処理
     .done(function(charge_result) {
       if(charge_result == 'error'){
-        alert('エラー');
+        alert('通信エラー');
+        $(this).prop('disabled', false).css("opacity", 1.0);
       }else{
         location.reload();
       }
