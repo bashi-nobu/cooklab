241731f0366a518a30afcf9b2463ee244e89ff51
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-16 16:15:18 +0900

    create contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_135455) do
+ActiveRecord::Schema.define(version: 2019_01_16_071011) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -86,6 +86,13 @@ ActiveRecord::Schema.define(version: 2019_01_15_135455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_chefs_on_name"
+  end
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notice_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
