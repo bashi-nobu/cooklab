34576d643c23eb46c67cf42d503fa25a443cf57d
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-01-16 22:40:53 +0900

    correction

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,7 +16,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.contact_mail(@contact).deliver
-      flash[:notice] = 'お問い合わせを受け付けました'
+      flash[:notice] = 'お問い合わせを受け付けました。'
       redirect_to root_path
     else
       render :new
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,6 @@ class ContactMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
   def contact_mail(contact)
     @contact = contact
-    # mail to: ENV['MAIL'], subject: "お客様からCOOK LABにお問い合わせがありました。"
-    mail to: 'sp2h5vb9ni@yahoo.co.jp', subject: "お客様からCOOK LABにお問い合わせがありました。"
+    mail to: Settings.contacts[:mail], subject: "お客様からCOOK LABにお問い合わせがありました。"
   end
 end
