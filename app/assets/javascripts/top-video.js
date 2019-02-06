62aedbf0712618a08dd1c8beb7126aa1ce5c9fa1
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-02-06 10:06:35 +0900

    correction

--- app/assets/javascripts/top-video.js
+++ app/assets/javascripts/top-video.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function(){
-  // var activeController = $('body').attr('data-controller');
-  // var activeAction = $('body').attr('data-action');
-  // var windowWidth = $(window).width();
-  // if (activeController == 'top' && activeAction == 'index' && windowWidth > 930) {
-  //   var html = '<video src="assets/top.mp4" id="bg-video" autoplay loop muted ></video>';
-  //   $('.main__logo').append(html);
-  // }
+  var activeController = $('body').attr('data-controller');
+  var activeAction = $('body').attr('data-action');
+  var windowWidth = $(window).width();
+  if (activeController == 'top' && activeAction == 'index' && windowWidth > 930) {
+    var html = '<video src="assets/top.mp4" id="bg-video" autoplay loop muted ></video>';
+    $('.main__logo').append(html);
+  }
 });
