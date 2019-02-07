e7a08db18a1f10335d431208062a252b5d4613c3
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2019-02-07 21:44:53 +0900

    correction

--- app/assets/javascripts/top-video.js
+++ app/assets/javascripts/top-video.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load', function(){
   if (activeController == 'top' && activeAction == 'index' && windowWidth > 930) {
     $('.top-slide').remove();
     $('.main__logo').css('padding-top','40%');
-    var html = '<div id="top_bg_video" class="player" data-property="{videoURL:\'https://player.vimeo.com/video/315642636\',containment:\'.main__logo\',autoPlay:true, mute:true, startAt:0, opacity:1, showControls:false}"></div>';
+    var html = '<div id="top_bg_video" class="player" data-property="{videoURL:\'https://player.vimeo.com/video/315863963\',containment:\'.main__logo\',autoPlay:true, mute:true, startAt:0, opacity:1, showControls:false}"></div>';
     $('.main__logo').append(html);
     $('#top_bg_video').vimeo_player();
   }
