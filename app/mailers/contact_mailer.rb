791e8c3dde4c42ec805d3c8ba95935a42cf5356b
nobu <sp2h5vb9ni@yahoo.co.jp>
2019-04-01 10:32:25 +0900

    edit contact mail to address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   add_template_helper(ApplicationHelper)
   def contact_mail(contact)
     @contact = contact
-    mail to: Settings.contacts[:mail], subject: "お客様からCOOK LABにお問い合わせがありました。"
+    mail to: 'info@restaurant-education.co.jp', subject: "お客様からCOOK LABにお問い合わせがありました。"
   end
 end
