57a0412edc335f176acdd560ac0add7287cb425e
nobu <sp2h5vb9ni@yahoo.co.jp>
2019-06-06 21:01:17 +0900

    set sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,17 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://cook-lab.net"
 
 SitemapGenerator::Sitemap.create do
+  add root_path            # root_pathをsitemapに追加する
+
+  # 各投稿をsitemapに追加する
+  Video.find_each do |video|
+    add video_path(video), priority: 1.0, lastmod: video.updated_at, changefreq: 'weekly'
+  end
+  Article.find_each do |article|
+    add article_path(article), priority: 1.0, lastmod: article.updated_at, changefreq: 'weekly'
+  end
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
