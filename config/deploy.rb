93e43bacfa8ba251760b029bd0138007ed563e0b
nobu <sp2h5vb9ni@yahoo.co.jp>
2019-06-12 00:22:46 +0900

    debug

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,18 +82,18 @@ namespace :deploy do
     end
   end
 
+  desc 'Generate sitemap'
+  task :sitemap do
+    on roles(:app) do
+      within release_path do
+        execute :bundle, :exec, :rake, 'sitemap:create RAILS_ENV=production'
+      end
+    end
+  end
+
   after  :migrate,      :seed
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-end
-
-namespace :sitemap do
-  desc "Generate sitemap.xml.gz"
-  task :generate do
-    on roles(:web) do
-      run "cd #{deploy_to}/current && /usr/bin/env bundle exec rake sitemap:refresh RAILS_ENV=#{rails_env}"
-    end
-  end
-  after "deploy:restart", "sitemap:generate"
+  after  :restart,      :sitemap
 end
