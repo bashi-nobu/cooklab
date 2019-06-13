c715b8a46d81c9f6d112456ffdc0d19454249de0
nobu <sp2h5vb9ni@yahoo.co.jp>
2019-06-13 22:34:13 +0900

    debug

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,12 +5,12 @@ SitemapGenerator::Sitemap.create do
   add root_path            # root_pathをsitemapに追加する
 
   # 各投稿をsitemapに追加する
-  Video.find_each do |video|
-    add video_path(video), priority: 1.0, lastmod: video.updated_at, changefreq: 'weekly'
-  end
-  Article.find_each do |article|
-    add article_path(article), priority: 1.0, lastmod: article.updated_at, changefreq: 'weekly'
-  end
+  # Video.find_each do |video|
+  #   add video_path(video), priority: 1.0, lastmod: video.updated_at, changefreq: 'weekly'
+  # end
+  # Article.find_each do |article|
+  #   add article_path(article), priority: 1.0, lastmod: article.updated_at, changefreq: 'weekly'
+  # end
 
   # Put links creation logic here.
   #
