1ac180a473c0516c1b09d8a16de13f67626fb13d
nobu <sp2h5vb9ni@yahoo.co.jp>
2019-09-23 20:54:56 +0900

    edit admin page(magazine_addresses)
    index一覧カラムにユーザーの会員種類を表示

--- app/admin/magazine_addresses.rb
+++ app/admin/magazine_addresses.rb
@@ -1,15 +1,14 @@
 ActiveAdmin.register MagazineAddress do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  index do
+    column :id
+    column :zipcode
+    column :pref
+    column :city_address
+    column :building
+    column '会員種類' do |ma|
+      ma.user.pay_regi_status
+    end
+    actions
+  end
 end
