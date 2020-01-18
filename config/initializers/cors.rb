59a4fb4c288eef1070c1494a02c1151470e70250
nobu <sp2h5vb9ni@yahoo.co.jp>
2020-01-18 15:52:55 +0900

    debug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://googleads.g.doubleclick.net'
+    # origins 'https://googleads.g.doubleclick.net'
+    origins "*"
     resource '*',
       headers: :any,
       methods: %i(get options head)
