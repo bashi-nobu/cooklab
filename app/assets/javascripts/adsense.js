9c46149f502135b40254d92bef65709f74af9e77
nobu <sp2h5vb9ni@yahoo.co.jp>
2020-01-19 15:14:01 +0900

    Update adsense.js

--- app/assets/javascripts/adsense.js
+++ app/assets/javascripts/adsense.js
@@ -1,16 +1,16 @@
-$(document).on('turbolinks:load', function(){
+// $(document).on('turbolinks:load', function(){
 
-  var ads = document.querySelectorAll('.adsbygoogle');
+//   var ads = document.querySelectorAll('.adsbygoogle');
 
-  if (ads.length > 0) {
-    ads.forEach(function (ad) {
-      if (ad.firstChild) {
-        ad.removeChild(ad.firstChild);
-      }
-    });
-    ads.forEach(function() {
-      window.adsbygoogle = window.adsbygoogle || [];
-      window.adsbygoogle.push({});
-    });
-  }
-});
+//   if (ads.length > 0) {
+//     ads.forEach(function (ad) {
+//       if (ad.firstChild) {
+//         ad.removeChild(ad.firstChild);
+//       }
+//     });
+//     ads.forEach(function() {
+//       window.adsbygoogle = window.adsbygoogle || [];
+//       window.adsbygoogle.push({});
+//     });
+//   }
+// });
